f0282a8352f04ac636c6ae8a4102a63302e20beb
Nolan <mrsonbot@gmail.com>
2026-09-23 17:09:42 +0200

    template ionic

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../usuario';
+import { Servicio } from '../servicio';
+import { IonContent, IonHeader, IonItem, IonLabel, IonTitle, IonToolbar } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss'],
-  imports: [],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel],
 })
 export class GaleriaComponent  implements OnInit {
   
@@ -12,8 +16,11 @@ export class GaleriaComponent  implements OnInit {
   public descripcion: string = 'Esta imagen se carga desde el directorio de assets mediante Property Binding';
   public rutaImagenLocal: string = 'assets/foto1.jpg';
 
-  constructor() {}
+  users:Usuario[]=[];
+  constructor(private usersService:Servicio) {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.users=await this.usersService.getUsuarios();
+  }
 
 }
--- src/app/servicio.ts
+++ src/app/servicio.ts
@@ -1,7 +1,10 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Usuario } from './usuario';
 
-@Service()
+@Injectable({
+    providedIn: 'root'
+})
+
 export class Servicio {
     private users: Usuario[] = [
         { id: 1, name: 'juan', email: 'juan@gmail.com', active: true },
